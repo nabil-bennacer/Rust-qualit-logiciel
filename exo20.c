b48b44ce1e554bb0f555d7961604f6164691cdc8
nabil <nabil.bennacer@etu.umontpellier.fr>
2026-02-12 10:03:24 +0100

    exo 20

---
+++ exo20.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void maybe_init(int init){
+    int a;
+    int b;
+    int c;
+
+    if (init == 1){
+        a = 42;
+        b = 2077;
+        c = 2029;
+    }
+
+    printf(" les valeurs de a,b,c sont :(%d,%d,%d) \n",a,b,c);
+    printf(" les valeurs de a,b,c sont :(%p,%p,%p) \n",&a,&b,&c);
+
+}
+
+int main(){
+    maybe_init(0);
+    maybe_init(1);
+    maybe_init(0);
+
+}
---
+++ exo21.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void *memcpy(void *src, void *dest, int size){
+    
+}
