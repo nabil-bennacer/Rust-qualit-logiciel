0e2431e0f5ab3c0bb9b4083dbe5280782cd55be2
nabil <nabil.bennacer@etu.umontpellier.fr>
2026-02-13 00:49:31 +0100

    exo21

--- exo21.c
+++ exo21.c
@@ -2,6 +2,57 @@
 #include <stdlib.h>
 #include <string.h>
 
-void *memcpy(void *src, void *dest, int size){
+void *memcpyNabil(void *src, void *dest, int size){
+    unsigned char *d = dest;
+    unsigned char *s = src;
+
+    for (int i = 0; i<size; i++){
+        d[i]=s[i];
+    }
+    return dest;
+}
+
+char *concat(char *str1, char *str2){
+    int taille1 = strlen(str1);
+    int taille2 = strlen(str2);
+    int tailleTot = taille1 + taille2 + 1;
+    char *result = malloc(tailleTot);
+    if (result==NULL){
+        return NULL;
+    }
+    memcpyNabil(str1,result, taille1);
+    memcpyNabil(str2, result + taille1,taille2 +1);
+    // equivalent à memcpyNabil(str2, &result[taille1],taille2 +1)
+
+    return result;
+}
+
+int main(int argc, char *argv[]){
+    // int t1[] = {1,2,3,4};
+    // int t2[4];
+
+    // memcpyNabil(t1, t2, sizeof(t1)); on copie tout le tableau
+ 
+    // for (int i=0;i<4;i++){
+    //     printf("%d ", t2[i]);
+    // }
+
+    int t1[] = {1,2,3,4};
+    int t2[3];
+
+    memcpyNabil(&t1[1],t2, 3*sizeof(int)); // Ici on va copier un sous tableau de t1
+
+    for (int i = 0; i<3; i++){
+        printf("%d ", t2[i]);
+    }
+    printf("\n");
+
+    char *a = "nab";
+    char *b ="test";
+
+    char * res = concat(a,b);
+    printf("%s \n",res);
+    free(res);
     
+
 }
